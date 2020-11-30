1d5ce3121fcd8203ce63fe58d273a2c8ab5eea5b
c0d3t3k <73602268+c0d3t3k@users.noreply.github.com>
2020-11-29 20:05:59 -0500

    Initial WIP Devtools

--- src/devtools.ts
+++ src/devtools.ts
@@ -1,6 +1,6 @@
 // import * as R from 'ramda';
 
-import {} from "valtio";
+import { subscribe } from "valtio";
 
 import { useAtom, WritableAtom } from "jotai";
 import { Observable, Subject, of } from "rxjs";
